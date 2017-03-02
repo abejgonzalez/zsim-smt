f5efd208bfd544390c188a50583892bfec7b738b
ronny <ronnymacmaster@gmail.com>
2017-03-02 13:32:25 -0600

    added libconfig1.5 install

--- configure.sh
+++ configure.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 set -x
+oooe_dir=`pwd`
+
 # extract tarballs
 libraries="
-pin3.2.tar.gz
-libconfig1.5.tar.gz"
+pin3.2
+libconfig1.5"
 mkdir -p lib/
 
 for lib in $libraries; do
-	tar zxf tar/$lib -C lib/
+	if [ ! -d lib/${lib} ] ; then
+		tar zxf tar/${lib}.tar.gz -C lib/
+	fi
 done
 
-# set the pinpath
-oooe_dir=`pwd`
+# setup pin
 export PINPATH=$oooe_dir/lib/pin3.2/
+
+# setup libconfig
+lcpath=lib/libconfig1.5
+export LIBCONFIGPATH=${oooe_dir}/${lcpath}/install
+
+if [ ! -d "lib/libconfig1.5/install" ] ; then
+	cd $lcpath
+	./configure --prefix=$LIBCONFIGPATH && make install
+	cd $oooe_dir
+fi
+
+# cleanup
+unset lcpath
+unset libraries
 unset oooe_dir
 set +x
