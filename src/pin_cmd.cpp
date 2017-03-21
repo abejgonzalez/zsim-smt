3d24e1e2afb2a42704e846af6690eeba13b9a183
abiegonzalez3169 <abiegonzalez96@gmail.com>
2017-03-21 14:19:13 -0500

    Workaround to make Pin work with 4.4 kernel

--- src/pin_cmd.cpp
+++ src/pin_cmd.cpp
@@ -52,6 +52,8 @@ PinCmd::PinCmd(Config* conf, const char* configFile, const char* outputDir, uint
     args.push_back(pinPath);
 
     //Global pin options
+    args.push_back("-injection");
+    args.push_back("child");
     args.push_back("-follow_execv"); //instrument child processes
     args.push_back("-tool_exit_timeout"); //don't wait much of internal threads
     args.push_back("1");
