a6fabe2d96c3bea9661d8db53e8a3c544fb8a905
Barak Lidsky <baraklidsky@yahoo.com>
2017-11-15 13:18:51 -0600

    try 2

--- sample_double_smt.sh
+++ sample_double_smt.sh
@@ -9,25 +9,6 @@ fi
 
 for ((i=0; i < $1; i++)); do
 	echo "/** sample: $i */"
-	make test
-	cp log/zsim.log.0 log/trial${i}_t0
-	cp log/zsim.log.1 log/trial${i}_t1
-	TotalCycle1=`awk ' BEGIN { x = 0 } $3 ~ /TotalCycle/ { x = $4; } \
-		END { print x; }' log/zsim.log.0 ` 
-	TotalCycle2=`awk ' BEGIN { x = 0 } $3 ~ /TotalCycle/ { x = $4; } \
-		END { print x; }' log/zsim.log.1 ` 
-	if [ "$TotalCycle1" -gt "$TotalCycle2" ]
-	then
-		echo thread1 $TotalCycle2, thread2 $TotalCycle1 >> tests/results.csv
-	fi
-	if [ "$TotalCycle2" -gt "$TotalCycle1" ]
-	then
-		echo thread1 $TotalCycle1, thread2 $TotalCycle2 >> tests/results.csv
-	fi
-
-done
-for ((i=0; i < $1; i++)); do
-	echo "/** sample: $i */"
 	make test1
 	cp log/zsim.log.0 log/trial${i}_t0
 	cp log/zsim.log.1 log/trial${i}_t1
